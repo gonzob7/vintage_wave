ef4f9a40b43fe00f428449c88770a72a1dbd2ed9
Gonzalo Birrueta <silentcr7@gmail.com>
2019-10-10 01:47:44 -0700

    app.py re did all routes

--- app.py
+++ app.py
@@ -1,20 +1,65 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+
+app = Flask(__name__, static_url_path='')
+
 
 client = MongoClient()
-db = client.vintagewave
-products = db.products
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/vintage'
+app.config['SECRET_KEY'] = 'clown123'
+mongo = PyMongo(app)
+db = mongo.db
 
-app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html')
+    items = db['items'].find()
+    return render_template('home.html', items=items)
 
-@app.route('/cart')
-def cart():
-    return render_template('cart.html', products=products.find())
+@app.route('/new')
+def new():
+    return render_template('new.html')
+
+
+@app.route('/delete/<itemId>')
+def playlists_delete(itemId):
+    db['items'].delete_one({'_id': ObjectId(itemId)})
+    return redirect('/')
+
+
+@app.route('/new', methods=['POST'])
+def post_new():
+    item = {
+        'name': request.form.get('name'),
+        'image': request.form.get('url'),
+        'price': request.form.get('price'),
+        'description': request.form.get('description')
+    }
+    db['items'].insert_one(item)
+    return redirect('/')
+
+
+@app.route('/edit/<itemId>')
+def edit(itemId):
+    item = db['items'].find_one({ '_id': ObjectId(itemId) })
+    return render_template('edit.html', item=item)
+
+@app.route('/edit/<itemId>', methods=['POST'])
+def edit_item(itemId):
+    updated_item = {
+        'name': request.form.get('name'),
+        'image': request.form.get('url'),
+        'price': request.form.get('price'),
+        'description': request.form.get('description')
+    }
+    db['items'].update_one(
+        {'_id': ObjectId(itemId)},
+        {'$set': updated_item})
+    return redirect('/')
 
 @app.route('/contact')
 def contact():
